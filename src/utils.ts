bb4ff88f85a0bf2a4e553b5dcacc0c422c16e307
Alvaro Furlan Falcao <contact@alvarofurlanfalcao.me>
2022-09-25 22:18:15 +0100

    Fixed a bug where time differences of over an hour would not be properly formatted

--- src/utils.ts
+++ src/utils.ts
@@ -5,20 +5,16 @@ const DAY = HOUR * 24
 export const formatUnixTimestamp = (unixTimestamp: number): string => {
     const secondsAgo = Math.round((new Date().valueOf() - unixTimestamp) / 1000)
 
-    if (secondsAgo > 1 * MINUTE) {
-        const minutesAgo = Math.round(secondsAgo/MINUTE)
-        return `${minutesAgo} ${minutesAgo === 1 ? 'minute' : 'minutes'} ago` 
-    }
-
-    if (secondsAgo > 1 * HOUR) {
-        const hoursAgo = Math.round(secondsAgo/HOUR)
-        return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago` 
-    }
-
     if (secondsAgo > 1 * DAY) {
-        const daysAgo = Math.round(secondsAgo/DAY)
+        const daysAgo = Math.round(secondsAgo / DAY)
         return `${daysAgo} ${daysAgo === 1 ? 'day' : 'days'} ago` 
+    } else if (secondsAgo > 1 * HOUR) {
+        const hoursAgo = Math.round(secondsAgo / HOUR)
+        return `${hoursAgo} ${hoursAgo === 1 ? 'hour' : 'hours'} ago` 
+    } else if (secondsAgo > 1 * MINUTE) {
+        const minutesAgo = Math.round(secondsAgo / MINUTE)
+        return `${minutesAgo} ${minutesAgo === 1 ? 'minute' : 'minutes'} ago` 
+    } else {
+        return `${secondsAgo} ${secondsAgo === 1 ? 'second' : 'seconds'} ago`
     }
-
-    return `${secondsAgo} ${secondsAgo === 1 ? 'second' : 'seconds'} ago`
 } 
