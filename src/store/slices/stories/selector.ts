6a2a11f29b5d527a4b101c382d3c453703f0ffb9
Alvaro Furlan Falcao <contact@alvarofurlanfalcao.me>
2022-09-26 11:27:42 +0100

    Updates

--- src/store/slices/stories/selector.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '../../store'
-
-export const selectStories = (state: RootState) => {
-    return state.stories.value
-}
--- src/store/slices/storyIds/selector.ts
+++
@@ -1,5 +0,0 @@
-import type { RootState } from '../../store'
-
-export const selectRecentStoryIds = (state: RootState) => state.storyIds.recent
-
-export const selectSavedStoryIds = (state: RootState) => state.storyIds.saved
